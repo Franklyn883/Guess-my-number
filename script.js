a9f41ac2665d002106c7a23af16b4f5486a1d165
Frank Alimimian <alimimianfrank@gmail.com>
2023-03-15 22:14:59 +0100

    Edit html, style and script I change the class names and ids in html and css, to make them more readable.

---
+++ script.js
@@ -0,0 +1,20 @@
+//cache the DOM
+const reset = document.getElementsByClassName('reset');
+//Game Levels
+const levelEasy = document.getElementById('easy');
+const levelNormal = document.getElementById('normal');
+const levelHard = document.getElementById('hard');
+
+let compGuess = document.getElementById('com-guess');
+let playerInput = document.getElementById('player-input');
+const checkBtn = document.getElementById('input-btn');
+let hintDisplay = document.getElementById('display-dashboard');
+let score = document.getElementById('score');
+let highScore = document.getElementById('high-score');
+
+const getCompChoice = function(){
+  let easy
+  
+}
+console.log(levelEasy)
+console.log('how are you  ')
