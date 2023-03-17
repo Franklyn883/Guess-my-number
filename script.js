e2658d5d5c7ac4cd63fe0b021c3dccd0248d90e3
Frank Alimimian <alimimianfrank@gmail.com>
2023-03-17 23:31:51 +0100

    Script: Add game modes

--- script.js
+++ script.js
@@ -5,16 +5,123 @@ const levelEasy = document.getElementById('easy');
 const levelNormal = document.getElementById('normal');
 const levelHard = document.getElementById('hard');
 
-let compGuess = document.getElementById('com-guess');
-let playerInput = document.getElementById('player-input');
-const checkBtn = document.getElementById('input-btn');
-let hintDisplay = document.getElementById('display-dashboard');
-let score = document.getElementById('score');
-let highScore = document.getElementById('high-score');
 
-const getCompChoice = function(){
-  let easy
+let compGuess = document.getElementById('comp-guess');
+let playerGuess = document.getElementById('player-input');
+const checkBtn = document.getElementById('check-btn');
+let display = document.getElementById('display-dashboard');
+let pointsDisplay = document.getElementById('points');
+let highScoreDisplay = document.getElementById('high-score');
+let numOfTriesDisplay = document.getElementById('tries')
+
+
+//State Variables
+ const compEasyGuess = Math.floor(Math.random()*20)+1;
+ const compNormalGuess = Math.floor(Math.random()*50)+1;
+ const compHardGuess = Math.floor(Math.random()*50)+1;
+let points = 0;
+let numOfTries = 10;
+let highScore = 0;
+
+ //function for possible guess outcome
+ const wrongGuess = function(){
+  compGuess.style.backgroundColor = 'red';
+  points; numOfTries --;
+  numOfTriesDisplay.textContent= numOfTries
   
+ }
+
+ //Game Logic for Easy Mode
+const easyGameMode = function(){
+  
+  const minNum = 1, maxNum =20;
+  
+ 
+  let playerGuessNum = Number(playerGuess.value);
+  if(!playerGuessNum || playerGuessNum < minNum || playerGuessNum > maxNum){
+    display.textContent = "🚫 Pick between 1 and 20";
+    playerGuess.style.borderColor = 'red';
+    points; numOfTries;  highScore;
+   
+    
+  }
+  else if(playerGuessNum > compEasyGuess){
+    display.textContent ="📈 Too high";
+   wrongGuess();
+  }
+  else if(playerGuessNum < compEasyGuess){
+    display.textContent ="📈 Too low"
+   wrongGuess();
+  }
+  else if(playerGuessNum === compEasyGuess){
+    display.textContent = '🥳 Correct Guess you Win'
+    compGuess.style.backgroundColor = 'green'
+    console.log(display.textContent)
+    points+=10
+    pointsDisplay.textContent=points
+  }
+  
+ 
+ 
 }
-console.log(levelEasy)
-console.log('how are you  ')
+
+const normalGameMode = function(){
+  const minNum = 1, maxNum = 50;
+  
+  console.log(display)
+  let playerGuessNum = Number(playerGuess.value);
+  if(!playerGuessNum || playerGuessNum < minNum || playerGuessNum > maxNum){
+    display.textContent = "🚫 Pick between 1 and 50";
+    playerGuess.style.borderColor = 'red';
+    points; numOfTries;  highScore;
+    
+    
+  }
+  else if(playerGuessNum > compEasyGuess){
+    display.textContent ="📈 Too high";
+   wrongGuess();
+  }
+  else if(playerGuessNum < compEasyGuess){
+    display.textContent ="📈 Too low"
+   wrongGuess();
+  }
+  else if(playerGuessNum === compEasyGuess){
+    display.textContent = '🥳 Correct Guess you Win'
+    compGuess.style.backgroundColor = 'green'
+   
+    points+=20
+    pointsDisplay.textContent=points
+  }
+ 
+}
+
+const hardGameMode = function(){
+  const minNum = 1, maxNum =100;
+  
+  console.log(display)
+  let playerGuessNum = Number(playerGuess.value);
+  if(!playerGuessNum || playerGuessNum < minNum || playerGuessNum > maxNum){
+    display.textContent = "🚫 Pick between 1 and 100";
+    playerGuess.style.borderColor = 'red';
+    points; numOfTries;  highScore;
+   
+    
+  }
+  else if(playerGuessNum > compEasyGuess){
+    display.textContent ="📈 Too high";
+   wrongGuess();
+  }
+  else if(playerGuessNum < compEasyGuess){
+    display.textContent ="📈 Too low"
+   wrongGuess();
+  }
+  else if(playerGuessNum === compEasyGuess){
+    display.textContent = '🥳 Correct Guess you Win'
+    compGuess.style.backgroundColor = 'green'
+    
+    points+=50
+    pointsDisplay.textContent=points
+  }
+}
+
+
